b207151052974c959222ddccb67c2cf345881602
Lailson Herondino <157296747+lailsoncode@users.noreply.github.com>
2024-10-14 17:20:19 +0000

    Aula 5

---
+++ js/script.js
@@ -0,0 +1,35 @@
+//alert('Hello Word')
+
+var i = 0
+
+while(i < 3) {
+    var numero = prompt("Digite sua nota: ");
+    console.log(i)
+    i++
+}
+
+
+// var numero = prompt("Digite sua nota: ");
+
+// var notaInt = parseInt(numero)
+
+// switch(notaInt){
+//     case 0:
+//     case 1:
+//     case 2:
+//     case 3:
+//     case 4:
+//     case 5:
+//     case 6:
+//         alert("Aprovado!")
+//         break
+//     case 7:
+//     case 8:
+//     case 9:
+//     case 10:
+//         alert("Aprovado!")
+//         break
+//     default:
+//         alert("Nota Inválida.")
+//         break
+// }
