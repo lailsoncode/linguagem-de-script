d86976da5c5ab6a013194935ead2d94ebd9c04f7
Lailson Herondino <157296747+lailsoncode@users.noreply.github.com>
2024-10-29 17:21:34 +0000

    Exercício  Tabuada com Functions

---
+++ js/functions.js
@@ -0,0 +1,57 @@
+function gerarTabuada(operacao){
+    switch(operacao){
+        case 'soma':
+            alert("Você Clicou no btn de soma")
+            for (i = 0; i < 10; i++) {
+                for (j = 0; j < 10; j++) {
+                document.getElementById("tabuada-soma").innerHTML += "<br />" + i + " + " + j + " = " + (i + j);
+            
+                if (j == 9) {
+                    document.getElementById("tabuada-soma").innerHTML += "<br />";
+                }
+                }
+            }
+            break;
+        case 'subtracao':
+            alert("Você Clicou no btn de subtracao")
+
+            for (i = 0; i < 10; i++) {
+                for (j = 0; j < 10; j++) {
+                document.getElementById("tabuada-sub").innerHTML += "<br />" + i + " - " + j + " = " + (i - j);
+            
+                if (j == 9) {
+                    document.getElementById("tabuada-sub").innerHTML += "<br />";
+                }
+                }
+            }
+            break;
+        case 'multiplicacao':
+            alert("Você Clicou no btn de multiplicacao")
+
+            for (i = 0; i < 10; i++) {
+                for (j = 0; j < 10; j++) {
+                document.getElementById("tabuada-div").innerHTML += "<br />" + i + " / " + j + " = " + (i / j);
+            
+                if (j == 9) {
+                    document.getElementById("tabuada-div").innerHTML += "<br />";
+                }
+                }
+            }
+            break;
+        case 'divisao':
+            alert("Você Clicou no btn de divisao")
+
+            for (i = 0; i < 10; i++) {
+                for (j = 0; j < 10; j++) {
+                document.getElementById("tabuada-mult").innerHTML += "<br />" + i + " * " + j + " = " + (i * j);
+            
+                if (j == 9) {
+                    document.getElementById("tabuada-mult").innerHTML += "<br />";
+                }
+                }
+            }
+            break;
+        default:
+            alert("Opção Inválida")
+    }
+}
--- js/tabuada.js
+++ js/tabuada.js
@@ -3,6 +3,10 @@
 for (i = 0; i < 10; i++) {
     for (j = 0; j < 10; j++) {
     document.getElementById("tabuada-soma").innerHTML += "<br />" + i + " + " + j + " = " + (i + j);
+
+    if (j == 9) {
+        document.getElementById("tabuada-soma").innerHTML += "<br />";
+    }
     }
 }
 
@@ -11,6 +15,10 @@ for (i = 0; i < 10; i++) {
 for (i = 0; i < 10; i++) {
     for (j = 0; j < 10; j++) {
     document.getElementById("tabuada-sub").innerHTML += "<br />" + i + " - " + j + " = " + (i - j);
+
+    if (j == 9) {
+        document.getElementById("tabuada-sub").innerHTML += "<br />";
+    }
     }
 }
 
@@ -19,6 +27,10 @@ for (i = 0; i < 10; i++) {
 for (i = 0; i < 10; i++) {
     for (j = 0; j < 10; j++) {
     document.getElementById("tabuada-div").innerHTML += "<br />" + i + " / " + j + " = " + (i / j);
+
+    if (j == 9) {
+        document.getElementById("tabuada-div").innerHTML += "<br />";
+    }
     }
 }
 
@@ -27,5 +39,9 @@ for (i = 0; i < 10; i++) {
 for (i = 0; i < 10; i++) {
     for (j = 0; j < 10; j++) {
     document.getElementById("tabuada-mult").innerHTML += "<br />" + i + " * " + j + " = " + (i * j);
+
+    if (j == 9) {
+        document.getElementById("tabuada-mult").innerHTML += "<br />";
+    }
     }
 }
