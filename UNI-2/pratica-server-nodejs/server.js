eb097c4ccab70df6bed6861a584c98691746cc05
Lailson Herondino <157296747+lailsoncode@users.noreply.github.com>
2024-12-10 17:18:11 +0000

    Praticar Node.Js

---
+++ UNI-2/pratica-server-nodejs/server.js
@@ -0,0 +1,43 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    const filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+    let extname = path.extname(filePath);
+
+    let contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+    }
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.end(content, 'utf8');
+                });
+            } else {
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`);
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf8');
+        }
+    });
+});
+const port = 3000
+
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
